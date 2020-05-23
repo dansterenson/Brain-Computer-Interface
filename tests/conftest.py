c350f1f7d5d74621fabd61e7cc53e109b0eba96f
Dan Sterenson <dansterenson@gmail.com>
2020-05-24 02:00:25 +0300

    Added documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import os
 import sys
+import pytest
+import pathlib
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append('../')
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+_SAMPLE_PATH = pathlib.Path(__file__).absolute().parent / 'tests/data/test_file.mind.gz'
