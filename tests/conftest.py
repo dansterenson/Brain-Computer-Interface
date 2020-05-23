16348945d664cb8cdcf6380e51b37f739ad928bb
Dan Sterenson <dansterenson@gmail.com>
2020-05-24 01:20:39 +0300

    Added documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,4 @@ import os
 import sys
 
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
