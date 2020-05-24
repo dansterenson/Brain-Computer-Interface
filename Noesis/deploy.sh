65ab1bae92b3dd97eeac6ec771166a00c139ccf3
Dan Sterenson <dansterenson@gmail.com>
2020-05-25 01:28:34 +0300

    working on gui docker

--- Noesis/deploy.sh
+++ Noesis/deploy.sh
@@ -1,12 +1,12 @@
 sudo docker volume create --name dataVolume1
 sudo docker network create containersnetwork
-#sudo docker run -d --name mongodb -p 27017:27017 --net containersnetwork mongo
+sudo docker run -d --name mongodb -p 27017:27017 --net containersnetwork mongo
 #sudo docker run -d --name rab -p 5672:5672 --net containersnetwork rabbitmq
 
 #sudo docker build -f server/Dockerfile -t server .
 #sudo docker build -t saver -f saver/Dockerfile .
 sudo docker build -t api -f api/Dockerfile .
-sudo docker build -f gui/Dockerfile -t gui .
+sudo docker build -t gui -f gui/Dockerfile .
 #sudo docker build -f parsers/Dockerfile -t pose --build-arg PARSER_NAME=pose .
 #sudo docker build -f parsers/Dockerfile -t color_image --build-arg PARSER_NAME=color_image .
 #sudo docker build -f parsers/Dockerfile -t depth_image --build-arg PARSER_NAME=depth_image .
@@ -14,15 +14,14 @@ sudo docker build -f gui/Dockerfile -t gui .
 
 sudo docker container ls -a
 
-#sudo docker stop api
-#sudo docker rm api
+sudo docker stop gui
+sudo docker rm gui
 
 sudo docker run -d --name api -p 5000:5000/tcp -v DataVolume1:/datavolume1 --net containersnetwork api
-sudo docker run -d --name gui -p 8080:8080/tcp -v
+sudo docker run -d --name gui -p 8080:8080/tcp gui
 #sudo docker run -d --name server -p 0.0.0.0:8000:8000/tcp -v DataVolume1:/datavolume1 --net containersnetwork server
 #sudo docker run -d --name saver --net containersnetwork saver
 #sudo docker run -d --name pose_parser --net containersnetwork pose
 #sudo docker run -d --name feelings_parser --net containersnetwork feelings
 #sudo docker run -d --name color_image_parser -v DataVolume1:/datavolume1 --net containersnetwork color_image
 #sudo docker run -d --name depth_image_parser -v DataVolume1:/datavolume1 --net containersnetwork depth_image
-
