515710c07bb44ea0f5da9180c422f3029611adc5
Dan Sterenson <dansterenson@gmail.com>
2020-05-31 15:26:02 +0300

    minor changes

--- Noesis/client/client.py
+++ Noesis/client/client.py
@@ -14,13 +14,9 @@ def upload_sample(address, file_path, reader_type='ProtoReader'):
     with reader as r:
         for snapshot in r:
             user = r.user
-            #with gzip.open('test_file.mind.gz', 'wb') as f:
-            #    user1 = user.SerializeToString()
-            #    snapshot1 = snapshot.SerializeToString()
-            #    f.write(len(user1).to_bytes(4, 'little') + user1 + len(snapshot1).to_bytes(4, 'little') + snapshot1)
             combined = protobuf_pb2.Combined(user=user, snapshot=snapshot).SerializeToString()
             requests.post(f'http://{host}:{port}/upload_snapshot', data=combined)
             snapshots_num += 1
-            logger.info(f'Uploaded user: {user}\n snapshot number: {snapshots_num}')
+            logger.info(f' Uploaded user({user.user_id}) and snapshot({snapshot.datetime}). number of snapshots uploaded: {snapshots_num}')
             logger.debug(f'Uploaded snapshot: {snapshot}')
         print(f"all {snapshots_num} snapshots of user_id: {user.user_id} were uploaded")
