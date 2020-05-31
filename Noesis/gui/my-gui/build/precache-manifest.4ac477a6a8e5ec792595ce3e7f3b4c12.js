8ecfa986abd2742af29821f9cb5c39b706b97f70
Dan Sterenson <dansterenson@gmail.com>
2020-05-31 17:42:51 +0300

    Added about page

---
+++ Noesis/gui/my-gui/build/precache-manifest.4ac477a6a8e5ec792595ce3e7f3b4c12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4186fecddf049f2d6876484d6b5375f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eae2f6489b5bcd50f40",
+    "url": "/static/css/2.bfa46653.chunk.css"
+  },
+  {
+    "revision": "b315335a9cb12868f726",
+    "url": "/static/css/main.0e407316.chunk.css"
+  },
+  {
+    "revision": "6eae2f6489b5bcd50f40",
+    "url": "/static/js/2.45a9b4b2.chunk.js"
+  },
+  {
+    "revision": "4c5c455bf71caf19eb17428c9fee791c",
+    "url": "/static/js/2.45a9b4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b315335a9cb12868f726",
+    "url": "/static/js/main.ef706f15.chunk.js"
+  },
+  {
+    "revision": "d49722d883c5238ec8e8",
+    "url": "/static/js/runtime-main.098b95e3.js"
+  },
+  {
+    "revision": "9aa8c43dc1681d6ca1e82a53403db40b",
+    "url": "/static/media/bci.9aa8c43d.jpg"
+  },
+  {
+    "revision": "7acc70e6fcd380d560261d14f780bd1a",
+    "url": "/static/media/home_icon.7acc70e6.png"
+  }
+]);
