9b8ce6d24a0a7fdd83ae77cb1339ece096a6d657
Dan Sterenson <dansterenson@gmail.com>
2020-05-31 14:42:13 +0300

    tests complete

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -5,7 +5,7 @@ import json
 from shutil import copyfile
 import os
 from subprocess import Popen, PIPE
-from Noesis import parsers
+import time
 
 
 @pytest.fixture
@@ -16,14 +16,15 @@ def file_data():
     return data
 
 
-#def test_parse_cli():
-#    message_path = str(pathlib.Path('tests/data/test_data.json'))
-#    process = Popen(["python", "-m", "Noesis.parsers", "parse", "pose", message_path], stdout=PIPE)
-#    (output, err) = process.communicate()
-#    assert b"{'user_info': {'user_id': 5, 'user_name': 'Dan Sterenson', 'birthday': 767404800, 'gender': 0}" in output
-#    assert b"'timestamp': '12345678'" in output
-#    assert b"result_name': 'pose'" in output
-#    assert b"'data': {'translation': [0.1, 0.2, 0.3], 'rotation': [0.1, 0.2, 0.3, 0.4]}" in output
+def test_parse_cli():
+    message_path = str(pathlib.Path('tests/data/test_data.json'))
+    process = Popen(["python", "-m", "Noesis.parsers", "parse", "pose", message_path], stdout=PIPE)
+    (output, err) = process.communicate()
+    time.sleep(2)
+    assert b"{'user_info': {'user_id': 5, 'user_name': 'Dan Sterenson', 'birthday': 767404800, 'gender': 0}" in output
+    assert b"'timestamp': '12345678'" in output
+    assert b"result_name': 'pose'" in output
+    assert b"'data': {'translation': [0.1, 0.2, 0.3], 'rotation': [0.1, 0.2, 0.3, 0.4]}" in output
 
 
 def test_feelings_parser(file_data):
